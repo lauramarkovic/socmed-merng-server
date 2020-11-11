1c92793fd22d507df86fac6e3511c163404b08d0
Laura Markovičová <itsmelararogers@MacBook-Air-uzivatela-Laura.local>
2020-11-11 13:18:28 +0100

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB: 'mongodb+srv://laurarogers:taminarossa133*@cluster0.6hfgv.mongodb.net/social_media_app?retryWrites=true&w=majority',
+  SECRET_KEY: "some very secret key"
+}
